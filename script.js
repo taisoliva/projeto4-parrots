cc1016a59e508b677964db4d4de82e9a0b881b29
taisoliva <tais15oliva@gmail.com>
2023-01-11 13:31:15 -0300

    Condição Inicial verificada

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 let face ;
 let verso ;
 
-const baralho = ["bobrossparrot.gif", "explodyparrot.gif"];
+let condicaoInicialCartas = false;
+
+let quantidadeDeCartas = prompt("Só pode inserir números pares de 4 a 14. \n Quantas cartas quer jogar:");
+
+while(condicaoInicialCartas === false){
+
+    if((quantidadeDeCartas % 2 == 0) && (quantidadeDeCartas > 3) && (quantidadeDeCartas < 15) ){
+        condicaoInicialCartas = true;
+    }else{
+        quantidadeDeCartas = prompt("Só pode inserir números pares de 4 a 14. \n Quantas cartas quer jogar:");
+    }
+}
+
+montarCartas(quantidadeDeCartas);
+
+function montarCartas (valorEscolhido){
+
+    const local = document.querySelector("tela");
+    local.innerHTML = "<div></div>";
+}
+
+
+
+
+const baralho = ["bobrossparrot.gif", "explodyparrot.gif", ];
 function selecionarCarta(carta){
     
     face = carta.querySelector ("div:nth-child(1)");
